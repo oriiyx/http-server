cb6fb3bfc76d5af0ec883a179568924bf67a39d3
Peter Paravinja <peter.paravinja@optiweb.com>
2024-06-01 19:05:01 +0200

    reading http connection

--- app/server.go
+++ app/server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net/http"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+)
+
+const (
+	IP = "4221"
 )
 
 func main() {
@@ -12,24 +19,46 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	address := "0.0.0.0:" + IP
+	l, err := net.Listen("tcp", address)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
+			fmt.Println("Could not close the listener: ", err.Error())
+			os.Exit(1)
+		}
+	}(l)
 
-	connection, err := l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	_, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	HandleRequest(conn)
+}
+
+func HandleRequest(conn net.Conn) {
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		return
+	}
+
+	if request.URL.Path != "" && request.URL.Path != "/" {
+		HandleConnWriting(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
+	HandleConnWriting(conn, "HTTP/1.1 200 OK\r\n\r\n")
+}
+
+func HandleConnWriting(conn net.Conn, response string) {
+	_, err := conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error writing back to connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	connection.Close()
 }
